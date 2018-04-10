9b95854bda68e8f89dc0514922a1f1f2f02d2b48
Dan Cecilio Chua <chua-d@cyscorpions.com>
2018-04-10 16:49:17 +0800

    Finish first app

--- MyApplication/app/src/main/java/com/example/danceciliochua/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/danceciliochua/myapplication/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.danceciliochua.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void toastMe(View view){
+        Toast mytoast = Toast.makeText(this,"Hello Toast!", Toast.LENGTH_LONG);
+        mytoast.show();
+    }
+
+    public void countMe(View view){
+        TextView showCountTextView = (TextView) findViewById(R.id.textView);
+        String countString = showCountTextView.getText().toString();
+        Integer count = Integer.parseInt(countString);
+        count++;
+        showCountTextView.setText(count.toString());
+    }
+
+    private static final String TOTAL_COUNT = "total_count";
+
+    public void randomMe(View view){
+        Intent randomIntent = new Intent(this, Main2Activity.class);
+        TextView showCountTextView = (TextView) findViewById( R.id.textView);
+
+        String stringCount = showCountTextView.getText().toString();
+
+        Integer count = Integer.parseInt(stringCount);
+
+        randomIntent.putExtra(TOTAL_COUNT,count);
+
+        startActivity(randomIntent);
+
+    }
+
+
 }
